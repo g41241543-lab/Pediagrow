174d758acd937815c3a6fefb7e71692d7d66b175
zaskiap <g41241272@student.polije.ac.id>
2026-09-15 16:58:07 +0700

    Changes to be committed: 	modified:   lib/features/pengguna/cek_stunting/services/stunting_ml_service.dart 	new file:   ml_backend/__pycache__/train_and_serve.cpython-314.pyc 	new file:   ml_backend/models/stunting_random_forest_best.joblib 	new file:   ml_backend/serve.py 	new file:   ml_backend/stunting.csv 	modified:   ml_backend/train_and_serve.py

---
+++ ml_backend/serve.py
@@ -0,0 +1,8 @@
+"""
+Launcher: melatih model (jika belum ada) lalu menjalankan REST API server.
+Jalankan: python serve.py
+"""
+from train_and_serve import run_rest_api_server
+
+if __name__ == "__main__":
+    run_rest_api_server()
