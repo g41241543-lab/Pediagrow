a266d5f6653552e1b1a5d4f18b7e137471879ab8
g41241509-beep <anitasty7@gmail.com>
2026-09-20 18:49:03 +0700

    Changes to be committed: 	modified:   macos/Flutter/GeneratedPluginRegistrant.swift 	modified:   windows/flutter/generated_plugin_registrant.cc 	modified:   windows/flutter/generated_plugins.cmake

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,13 +7,10 @@ import Foundation
 
 import cloud_firestore
 import file_selector_macos
-<<<<<<< HEAD
 import firebase_core
 import google_sign_in_ios
-=======
 import printing
 import shared_preferences_foundation
->>>>>>> 7e57e7ab9ceb4eb2352799a5200f15f46a478e0f
 import sqflite_darwin
 import url_launcher_macos
 import webview_flutter_wkwebview
@@ -21,13 +18,10 @@ import webview_flutter_wkwebview
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
-<<<<<<< HEAD
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
-=======
   PrintingPlugin.register(with: registry.registrar(forPlugin: "PrintingPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
->>>>>>> 7e57e7ab9ceb4eb2352799a5200f15f46a478e0f
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
   WebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "WebViewFlutterPlugin"))
--- windows/flutter/generated_plugin_registrant.cc
+++ windows/flutter/generated_plugin_registrant.cc
@@ -8,11 +8,8 @@
 
 #include <cloud_firestore/cloud_firestore_plugin_c_api.h>
 #include <file_selector_windows/file_selector_windows.h>
-<<<<<<< HEAD
 #include <firebase_core/firebase_core_plugin_c_api.h>
-=======
 #include <printing/printing_plugin.h>
->>>>>>> 7e57e7ab9ceb4eb2352799a5200f15f46a478e0f
 #include <url_launcher_windows/url_launcher_windows.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
@@ -20,13 +17,10 @@ void RegisterPlugins(flutter::PluginRegistry* registry) {
       registry->GetRegistrarForPlugin("CloudFirestorePluginCApi"));
   FileSelectorWindowsRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("FileSelectorWindows"));
-<<<<<<< HEAD
   FirebaseCorePluginCApiRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("FirebaseCorePluginCApi"));
-=======
   PrintingPluginRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("PrintingPlugin"));
->>>>>>> 7e57e7ab9ceb4eb2352799a5200f15f46a478e0f
   UrlLauncherWindowsRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("UrlLauncherWindows"));
 }
